c6f4019303641394695c5013218244edbffda109
JangMyungJoo <wkdaudwn1028@gmail.com>
2021-03-03 18:28:30 +0900

    SimpleTodo) Feat: Todo 추가, 삭제 기능 구현

--- SimpleTodo/App.js
+++ SimpleTodo/App.js
@@ -1,9 +1,15 @@
-
-
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components/native";
+import { Platform } from "react-native";
+import Constants from "expo-constants";
+import _ from "lodash";
 
 const Container = styled.SafeAreaView`
+  flex: 1;
+  padding-top: ${Constants.statusBarHeight}px;
+`;
+
+const KeyboardAvoidingView = styled.KeyboardAvoidingView`
   flex: 1;
 `;
 
@@ -16,6 +22,7 @@ const Contents = styled.ScrollView`
 const TodoItem = styled.View`
   flex-direction: row;
   align-items: center;
+  padding: 8px 0;
 `;
 const TodoItemText = styled.Text`
   font-size: 20px;
@@ -36,18 +43,60 @@ const Input = styled.TextInput`
 const Button = styled.Button``;
 
 export default function App() {
+  const [input, setInput] = React.useState("");
+  const [list, setList] = React.useState([
+    {
+      id: 1,
+      todo: "할 일 1"
+    },
+    {
+      id: 2,
+      todo: "할 일 2"
+    },
+    {
+      id: 3,
+      todo: "할 일 3"
+    }
+  ]);
+
+  function handleAddPress(todo) {
+    if (!todo) return;
+
+    const newItem = {
+      id: new Date().getTime().toString(),
+      todo
+    };
+    setList([...list, newItem]);
+    setInput("");
+  }
+
+  function handleDeletePress(id) {
+    // const newList = list.filter(item => item.id !== id);
+    // setList(newList);
+    const newList = _.reject(list, item => item.id === id);
+    setList(newList);
+  }
+
   return (
     <Container>
-      <Contents>
-        <TodoItem>
-          <TodoItemText>할 일 목록</TodoItemText>
-          <TodoItemButton title="삭제" onPress={null} />
-        </TodoItem>
-      </Contents>
-      <InputContainer>
-        <Input />
-        <Button title="전송" onPress={null} />
-      </InputContainer>
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
+        <Contents>
+          {list.map(item => {
+            const { id, todo } = item;
+            return (
+              <TodoItem key={id}>
+                <TodoItemText>{todo}</TodoItemText>
+                <TodoItemButton title="삭제" onPress={() => handleDeletePress(id)} />
+              </TodoItem>
+            );
+          })}
+        </Contents>
+
+        <InputContainer>
+          <Input value={input} onChangeText={value => setInput(value)} />
+          <Button title="전송" onPress={() => handleAddPress(input)} />
+        </InputContainer>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
