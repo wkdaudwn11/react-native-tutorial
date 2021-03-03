0711014a34d3659314087e932af701ff7185b16e
JangMyungJoo <wkdaudwn1028@gmail.com>
2021-03-03 12:54:53 +0900

    SimpleTodo) Feat: 간단한 UI 작성

--- SimpleTodo/App.js
+++ SimpleTodo/App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
+
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components';
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+const Contents = styled.ScrollView`
+  flex: 1;
+  padding: 8px 24px;
+  background-color: #efefef;
+`;
+
+const TodoItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+const TodoItemText = styled.Text`
+  font-size: 20px;
+  flex: 1;
+`;
+const TodoItemButton = styled.Button``;
+
+const InputContainer = styled.View`
+  flex-direction: row;
+  padding: 8px 24px;
+`;
+
+const Input = styled.TextInput`
+  border: 1px solid #a0a0a0;
+  flex: 1;
+`;
+
+const Button = styled.Button``;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Contents>
+        <TodoItem>
+          <TodoItemText>할 일 목록</TodoItemText>
+          <TodoItemButton title="삭제" onPress={null} />
+        </TodoItem>
+      </Contents>
+      <InputContainer>
+        <Input />
+        <Button title="전송" onPress={null} />
+      </InputContainer>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
