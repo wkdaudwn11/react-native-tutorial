6c21f5982d7f73eb4a4af07c9081bad3b5ef7822
JangMyungJoo <wkdaudwn1028@gmail.com>
2021-03-11 17:02:50 +0900

    SongFinder) Feat: 기본적인 UI 구성

--- SongFinder/App.js
+++ SongFinder/App.js
@@ -1,5 +1,58 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function App() {}
+const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+const Contents = styled.ScrollView`
+  flex: 1;
+`;
+
+const Row = styled.View`
+  flex-direction: row;
+  padding: 0 12px;
+  margin-bottom: 12px;
+`;
+
+const Input = styled.TextInput`
+  flex: 1;
+  border: 1px solid #e5e5e5;
+  padding: 8px;
+`;
+
+const Button = styled.Button``;
+
+const ListItem = styled.TouchableOpacity`
+  padding: 6px 12px;
+  border-bottom-color: #e5e5e5;
+  border-bottom-width: 1px;
+`;
+
+const Label = styled.Text`
+  font-size: 16px;
+`;
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Input />
+        <Button title="검색" />
+      </Row>
+      <Contents>
+        <ListItem>
+          <Label>검색 결과</Label>
+        </ListItem>
+        <ListItem>
+          <Label>검색 결과</Label>
+        </ListItem>
+        <ListItem>
+          <Label>검색 결과</Label>
+        </ListItem>
+      </Contents>
+    </Container>
+  );
+}
 
 export default App;
