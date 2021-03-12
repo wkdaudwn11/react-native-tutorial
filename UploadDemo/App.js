45bb84c9c23574ee11ce64488644af411368ccbc
JangMyungJoo <wkdaudwn1028@gmail.com>
2021-03-12 15:27:36 +0900

    UploadDemo) Fix: 파일 업로드 안되던 버그 수정

--- UploadDemo/App.js
+++ UploadDemo/App.js
@@ -9,12 +9,13 @@
 import React from 'react';
 import {
   SafeAreaView,
+  Text,
   ActivityIndicator,
   Image,
   Button,
   StatusBar,
 } from 'react-native';
-import ImagePicker from 'react-native-image-picker';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import axios from 'axios';
 
 const options = {
@@ -26,62 +27,47 @@ const options = {
   },
 };
 
-const config = {
-  headers: {
-    Authorization: 'Client-ID 21e459cc3b8bb2c',
-  },
-};
-
 function App() {
+  const [fileName, setFileName] = React.useState(null);
   const [url, setUrl] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(false);
 
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
+      <SafeAreaView
+        style={{flex: 1, justifyContent: 'center', alignContent: 'center'}}>
         <Button
           title="이미지 선택"
           onPress={() => {
-            setIsLoading(true);
-            ImagePicker.showImagePicker(options, (response) => {
-              console.log('Response = ', response);
+            try {
+              setIsLoading(true);
 
-              if (response.didCancel) {
-                setIsLoading(false);
-                console.warn('User cancelled image picker');
-              } else if (response.error) {
-                setIsLoading(false);
-                console.warn('ImagePicker Error: ', response.error);
-                alert('ImagePicker Error: ' + response.error);
-              } else if (response.customButton) {
-                setIsLoading(false);
-                console.warn(
-                  'User tapped custom button: ',
-                  response.customButton,
-                );
-              } else {
-                // const source = { uri: response.uri };
+              launchImageLibrary(null, (response) => {
+                console.log('Response = ', response);
 
-                // You can also display the image using data:
-                // const source = { uri: 'data:image/jpeg;base64,' + response.data };
-                // setUrl( 'data:' + response.type + ';base64,' + response.data );
-                const params = new FormData();
-                params.append('image', response.data);
-                axios
-                  .post('https://api.imgur.com/3/image', params, config)
-                  .then((response) => {
-                    setUrl(response.data.data.link);
-                  })
-                  .catch((error) => {
-                    console.warn(error);
-                    alert('Error : ' + error.response.data.data.error);
-                  })
-                  .finally(() => {
-                    setIsLoading(false);
-                  });
-              }
-            });
+                if (response.didCancel) {
+                  setIsLoading(false);
+                  console.warn('User cancelled image picker');
+                } else if (response.error) {
+                  setIsLoading(false);
+                  console.warn('ImagePicker Error: ', response.error);
+                  alert('ImagePicker Error: ' + response.error);
+                } else if (response.customButton) {
+                  setIsLoading(false);
+                  console.warn(
+                    'User tapped custom button: ',
+                    response.customButton,
+                  );
+                } else {
+                  setFileName(response.fileName);
+                  setUrl(response.uri);
+                  setIsLoading(false);
+                }
+              });
+            } catch (e) {
+              alert(e.message);
+            }
           }}
         />
 
@@ -90,7 +76,10 @@ function App() {
         ) : (
           <>
             {url && (
-              <Image source={{uri: url}} style={{width: 340, height: 340}} />
+              <>
+                <Text>{fileName}</Text>
+                <Image source={{uri: url}} style={{width: 340, height: 340}} />
+              </>
             )}
           </>
         )}
